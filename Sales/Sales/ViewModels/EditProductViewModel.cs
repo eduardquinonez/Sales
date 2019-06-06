6991672f83d653e45825e6b22de5c96029e74f9b
eduardquinonez <eduardquinonez@users.noreply.github.com>
2019-06-06 17:09:49 -0500

    Edit Product Complete

--- Sales/Sales/ViewModels/EditProductViewModel.cs
+++ Sales/Sales/ViewModels/EditProductViewModel.cs
@@ -1,18 +1,208 @@
 ﻿namespace Sales.ViewModels
 {
+    using System.Linq;
+    using System.Windows.Input;
     using Common.Models;
+    using GalaSoft.MvvmLight.Command;
+    using Helpers;
+    using Plugin.Media;
+    using Plugin.Media.Abstractions;
+    using Services;
+    using Xamarin.Forms;
 
-    public class EditProductViewModel
+    public class EditProductViewModel : BaseViewModel
     {
         #region Attributes
+
         private Product product;
+        private MediaFile file; //Captura las fotos en memoria
+        private ImageSource imageSource;
+        private ApiService apiService;
+        private bool isRunning;
+        private bool isEnabled;
+
+        #endregion
+
+        #region Properties
+        public Product Product
+        {
+            get { return this.product; }
+            set { this.SetValue(ref this.product, value); }
+        }
+
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { this.SetValue(ref this.isRunning, value); }
+        }
+
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { this.SetValue(ref this.isEnabled, value); }
+        }
+
+        public ImageSource ImageSource
+        {
+            get { return this.imageSource; }
+            set { this.SetValue(ref this.imageSource, value); }
+        }
         #endregion
 
         #region Constructors
         public EditProductViewModel(Product product)
         {
             this.product = product;
-        } 
+            this.apiService = new ApiService();
+            this.isEnabled = true;
+            this.ImageSource = product.ImageFullPath;
+        }
         #endregion
+
+
+        #region Commands
+
+        public ICommand ChangeImageCommand
+        {
+            get
+            {
+                return new RelayCommand(ChangeImage);
+            }
+        }
+        private async void ChangeImage()
+        {
+            //Inicializar la libreria de Fotos
+            await CrossMedia.Current.Initialize();
+
+            var source = await Application.Current.MainPage.DisplayActionSheet(
+                Languages.ImageSource,
+                Languages.Cancel,
+                null,
+                Languages.FromGallery,
+                Languages.NewPicture);
+
+            if (source == Languages.Cancel)
+            {
+                // No Hay Foto
+                this.file = null;
+                return;
+            }
+
+            if (source == Languages.NewPicture)
+            {
+                this.file = await CrossMedia.Current.TakePhotoAsync(
+                    new StoreCameraMediaOptions
+                    {
+                        Directory = "Sample",
+                        Name = "test.jpg",
+                        PhotoSize = PhotoSize.Small,
+                    }
+                );
+            }
+            else
+            {
+                this.file = await CrossMedia.Current.PickPhotoAsync();
+            }
+
+            if (this.file != null)
+            {
+                this.ImageSource = ImageSource.FromStream(() =>
+                {
+                    var stream = this.file.GetStream();
+                    return stream;
+                });
+            }
+        }
+
+        public ICommand SaveCommand
+        {
+            get
+            {
+                return new RelayCommand(Save);
+            }
+        }
+
+        private async void Save()
+        {
+            if (string.IsNullOrEmpty(this.product.Description))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.DescriptionError,
+                    Languages.Accept);
+                return;
+            }
+
+            if (this.product.Price < 0)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.PriceError,
+                    Languages.Accept);
+                return;
+            }
+
+            // Deshabilitar Activity Indicator y el Botón Save
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            // Validar conexión a internet en el celular
+            var connection = await this.apiService.CheckConnection();
+            if (!connection.IsSuccess)
+            {
+                // Deshabilitar Activity Indicator y el Botón Save
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    connection.Message,
+                    Languages.Accept);
+                return;
+            }
+
+            //Validar si el usuario escogió foto
+            byte[] imageArray = null;
+            if (this.file != null)
+            {
+                imageArray = FileHelper.Readfully(this.file.GetStream());
+                this.Product.ImageArray = imageArray;
+            }
+
+            //Conectarse al servicio API
+            var url = Application.Current.Resources["UrlAPI"].ToString();
+            var prefix = Application.Current.Resources["UrlPrefix"].ToString();
+            var controller = Application.Current.Resources["UrlProductsController"].ToString();
+            var response = await this.apiService.Put(url, prefix, controller, this.Product, this.Product.ProductId);
+
+            //Validar si lo grabó...
+            if (!response.IsSuccess)
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    response.Message,
+                    Languages.Accept);
+                return;
+            }
+
+            // Usar el response de producto para agregar el nuevo producto
+            var newProduct = (Product)response.Result;
+            var productsViewModel = ProductsViewModel.GetInstance();
+            var oldProduct = productsViewModel.MyProducts.Where(p => p.ProductId == this.Product.ProductId).FirstOrDefault();
+            if (oldProduct != null)
+            {
+                productsViewModel.MyProducts.Remove(oldProduct);
+            }
+            productsViewModel.MyProducts.Add(newProduct);
+            productsViewModel.RefreshList();
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+            await Application.Current.MainPage.Navigation.PopAsync();
+
+        }
+        #endregion
+
     }
 }
